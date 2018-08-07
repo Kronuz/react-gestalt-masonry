acfc527a3ec365af97a81eb4dab22d37dc3aaacd
German M. Bravo <german.mb@gmail.com>
2018-08-07 12:48:32 -0500

    Update earlyGridRender

--- earlyGridRender.js
+++ earlyGridRender.js
@@ -1,30 +1,33 @@
-function getShortestColumn(colHeights) {
-    var shortestIndex = 0;
-    var minHeight = colHeights[0];
-    for (var i = 1; i < colHeights.length; i++) {
-        if (colHeights[i] < colHeights[shortestIndex]) {
-            shortestIndex = i;
-            minHeight = colHeights[i];
-        }
-    }
-    return shortestIndex;
-}
 try {
-    var pins = [].slice.call(document.getElementsByClassName('static'));
-    var gridWidth = document.getElementsByClassName('gridCentered')[0].clientWidth;
-    var pinWidth = pins[0].clientWidth;
-    var numCols = gridWidth / pinWidth;
-    var colHeights = new Array(numCols);
-    for (var i = 0; i < pins.length; i++) {
-        if (i < numCols) {
-            colHeights[i] = 0;
-        }
-        var pin = pins[i];
-        var shortestColumn = getShortestColumn(colHeights);
-        pin.style.top = colHeights[shortestColumn] + 'px';
-        pin.style.left = shortestColumn * pin.clientWidth + 'px';
-        pin.style.visibility = 'visible';
-        colHeights[shortestColumn] = colHeights[shortestColumn] + pin.clientHeight;
-    }
-    window.didEarlyGridRender = true;
+	function getShortestColumn(colHeights) {
+		var shortestIndex = 0;
+		var minHeight = colHeights[0];
+		for (var i = 1; i < colHeights.length; i++) {
+			if (colHeights[i] < colHeights[shortestIndex]) {
+				shortestIndex = i;
+				minHeight = colHeights[i];
+			}
+		}
+		return shortestIndex;
+	}
+	var grids = document.getElementsByClassName('Masonry');
+	for (var j = 0; j < grids.length; j++) {
+		var grid = grids[j];
+		var gridWidth = grid.clientWidth;
+		var pins = [].slice.call(grid.getElementsByClassName('static'));
+		var pinWidth = pins[0].clientWidth;
+		var numCols = gridWidth / pinWidth;
+		var colHeights = new Array(numCols);
+		for (var i = 0; i < pins.length; i++) {
+			if (i < numCols) {
+				colHeights[i] = 0;
+			}
+			var pin = pins[i];
+			var shortestColumn = getShortestColumn(colHeights);
+			pin.style.top = colHeights[shortestColumn] + 'px';
+			pin.style.left = shortestColumn * pin.clientWidth + 'px';
+			pin.style.visibility = 'visible';
+			colHeights[shortestColumn] = colHeights[shortestColumn] + pin.clientHeight;
+		}
+	}
 } catch (e) {}
